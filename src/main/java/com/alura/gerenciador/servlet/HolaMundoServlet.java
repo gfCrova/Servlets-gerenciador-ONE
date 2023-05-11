ac54cdf19ea7cb2d236c4e0296c35179045eae17
gfCrova <giancrova.cl5@gmail.com>
2023-05-11 20:43:42 -0300

    add new proyect alura servlet

---
+++ src/main/java/com/alura/gerenciador/servlet/HolaMundoServlet.java
@@ -0,0 +1,32 @@
+package com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet(urlPatterns = "/saludo")
+public class HolaMundoServlet extends HttpServlet {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Hola Mundo!!");
+		out.println("</body>");
+		out.println("</html>");
+		
+	}
+	
+}
