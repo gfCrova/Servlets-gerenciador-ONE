a629394dbe8aba953dcf30677d9f3b092ab4038f
gfCrova <giancrova.cl5@gmail.com>
2023-05-12 01:33:49 -0300

    change method service for doPost

--- src/main/java/com/alura/gerenciador/servlet/NuevaEmpresaServlet.java
+++ src/main/java/com/alura/gerenciador/servlet/NuevaEmpresaServlet.java
@@ -15,7 +15,7 @@ public class NuevaEmpresaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
    
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Nueva empresa registrada");
 		
 		String nombreEmpresa = request.getParameter("nombre");
