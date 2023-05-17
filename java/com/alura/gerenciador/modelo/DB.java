49bbeab7bf6cc7e0e0c1475650caf29a4a1a4ba2
gfCrova <giancrova.cl5@gmail.com>
2023-05-17 00:23:52 -0300

    new class Usuario, instance usuario en DB

--- java/com/alura/gerenciador/modelo/DB.java
+++ java/com/alura/gerenciador/modelo/DB.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class DB {
 	
 	private static List<Empresa> listaDeEmpresas = new ArrayList<>();
+	private static List<Usuario> listaDeUsuarios = new ArrayList<>();
 	private static Integer llaveSecuencial = 1;
 	
 	static {
@@ -18,6 +19,15 @@ public class DB {
 		empresa2.setId(DB.llaveSecuencial++);
 		listaDeEmpresas.add(empresa1);
 		listaDeEmpresas.add(empresa2);
+		
+		Usuario u1 = new Usuario();
+		Usuario u2 = new Usuario();
+		u1.setLogin("Carlos");
+		u2.setLogin("Laura");
+		u1.setPassword("12345");
+		u2.setPassword("12345");
+		listaDeUsuarios.add(u1);
+		listaDeUsuarios.add(u2);
 	}
 	
 	public void agregarEmpresa(Empresa empresa) {
@@ -47,6 +57,16 @@ public class DB {
 		for (Empresa empresa : listaDeEmpresas) {
 			if(empresa.getId() == id) {
 				return empresa;
+			}
+		}
+		return null;
+	}
+
+
+	public Usuario existeUsuario(String paramLogin, String paramPassword) {
+		for (Usuario usuario : listaDeUsuarios) {
+			if(usuario.esIgual(paramLogin, paramPassword)) {
+				return usuario;
 			}
 		}
 		return null;
