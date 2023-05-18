166df5a5eedb06589d4339865f53f21ced5b6894
gfCrova <giancrova.cl5@gmail.com>
2023-05-18 00:40:49 -0300

    new accion logout

---
+++ java/com/alura/gerenciador/accion/Logout.java
@@ -0,0 +1,22 @@
+package com.alura.gerenciador.accion;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class Logout implements Accion {
+
+	@Override
+	public String ejecutar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		HttpSession sesion = request.getSession();
+		//sesion.removeAttribute("LoginUsuario");
+		sesion.invalidate();
+		
+		return "redirect:entrada?accion=FormLogin";
+	}
+
+}
